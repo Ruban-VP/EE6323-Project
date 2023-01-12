b5cd116f8c02c67168515aa57096ac5aec196f70
Ruban-VP <64304414+Ruban-VP@users.noreply.github.com>
2023-01-12 22:48:55 +0530

    Add files via upload
    Updated the baseband transmitter codes and modularized the functions

---
+++ Tx baseband/BB_chain.m
@@ -0,0 +1,22 @@
+function [Tx_out] = BB_chain(M,N_subcar,N_FFT,N_CP,N_OFDM_sym)
+
+N_sym = N_subcar*N_OFDM_sym;
+Bits = randi([0,1],M,N_sym);            
+Xmod = qammod(Bits,2^M,"gray","InputType","bit",UnitaveragePower=true);
+Xmod_resh = reshape(Xmod,N_subcar,N_OFDM_sym);
+
+% Place symbols on carriers such that index 0 symbol is placed on the central carrier 
+kk1 = 1:N_subcar/2;
+kk2 = N_FFT-(N_subcar/2)+1:N_FFT;
+
+X_shift = zeros(N_FFT,N_OFDM_sym);
+X_shift(kk1,:) = Xmod_resh((N_subcar/2)+1:N_subcar,:);
+X_shift(kk2,:) = Xmod_resh(1:N_subcar/2,:);
+% Mount symbols on carriers
+
+X_ifft = sqrt(N_FFT)*ifft(X_shift);              % IFFT operation 
+X_cp = [X_ifft(N_FFT-N_CP+1:N_FFT,:); X_ifft];   % Addition of Cyclic Prefix
+
+Tx_out = reshape(X_cp,1,(N_FFT+N_CP)*N_OFDM_sym);
+
+end
---
+++ Tx baseband/Baseband_Tx.m
@@ -0,0 +1,25 @@
+%% EE6323: Project
+% Baseband transmitter
+
+%% Parameter initializations
+
+M = 4;
+N_slots = 10;
+N_OFDM_sym = 14*N_slots;
+N_PRBs = 273;
+N_subcar = 12*N_PRBs;
+N_CP = 288;
+N_FFT = 2^ceil(log2(N_subcar));
+
+%% Baseband transmitter chain
+
+Tx_out = BB_chain(M,N_subcar,N_FFT,N_CP,N_OFDM_sym);
+I_vals = real(Tx_out);
+Q_vals = imag(Tx_out);
+
+%% Converting data to finite precision
+
+V_ref = 3.3;
+num_bits = 16;
+I_quant = Bitconvert(I_vals,V_ref,num_bits);
+Q_quant = Bitconvert(Q_vals,V_ref,num_bits);
