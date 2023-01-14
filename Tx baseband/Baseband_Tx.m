54024f5415efa6a9177fcb94ae0c55755556cf06
Ruban-VP <64304414+Ruban-VP@users.noreply.github.com>
2023-01-14 16:07:32 +0530

    Updated the codes

--- Tx baseband/Baseband_Tx.m
+++ Tx baseband/Baseband_Tx.m
@@ -2,20 +2,27 @@
 % Baseband transmitter
 
 %% Parameter initializations
-
-M = 4;
-N_slots = 10;
-N_OFDM_sym = 14*N_slots;
-N_PRBs = 273;
-N_subcar = 12*N_PRBs;
-N_CP = 288;
-N_FFT = 2^ceil(log2(N_subcar));
+ 
+M = 4;                           % Modulation index
+N_slots = 10;                    % N0. of slots
+N_OFDM_sym = 14*N_slots;         % No. of OFDM symbols
+N_PRBs = 273;                    % No. of PRBs occupied
+N_subcar = 12*N_PRBs;            % No. of subcarriers occupied 
+N_CP = 288;                      % Cyclic prefix length 
+N_FFT = 2^ceil(log2(N_subcar));  % Minimum FFT length required
+fs = N_FFT*30e-3;                % Sampling frequency in MHz
 
 %% Baseband transmitter chain
 
 [Tx_bits, Tx_syms, Tx_out] = BB_Tx_chain(M,N_subcar,N_FFT,N_CP,N_OFDM_sym);
 I_vals = real(Tx_out);
 Q_vals = imag(Tx_out);
+
+figure(1)
+pspectrum(Tx_out,fs);
+xlabel('Frequency (in MHz)');
+ylabel('PSD (in dBm/Hz)');
+title('PSD plot of Tx signal');
 
 %% Converting data to finite precision
 
